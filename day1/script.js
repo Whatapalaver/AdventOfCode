d61abef21e492010fbf4c20db1b679ab668494c4
Angela Wolff <warriorwomenblog@gmail.com>
2018-12-01 12:12:36 +0000

    Completed part 2 of the Day 1 solution

--- day1/script.js
+++ day1/script.js
@@ -1,12 +1,41 @@
 const fs = require('fs');
 
-function day1() {
+// PART 1
+
+function day1part1() {
   fs.readFile('./frequency.txt', (err, data) => {
     const frequencyShift = data.toString();
     const frequencyShiftArray = frequencyShift.split('\n').map(Number);
     let sum = frequencyShiftArray.reduce((a, b) => a + b, 0);
     console.log(sum);
+    return sum;
   })
 }
 
-day1()
+// PART 2
+
+function day1part2() {
+  fs.readFile('./frequency.txt', (err, data) => {
+    const frequencyShift = data.toString();
+    const frequencyShiftArray = frequencyShift.split(/\s/).map(Number);
+    let cumulativeFrequencyArray = [0];
+    let currentFrequency = 0;
+    let repeated = false;
+  
+    while (repeated === false) {
+      frequencyShiftArray.forEach(frequencyChange => {
+        currentFrequency += frequencyChange;
+        if (cumulativeFrequencyArray.includes(currentFrequency) && repeated === false) {
+          console.log('Repeat of: ', currentFrequency);
+          repeated = true;
+        } else {
+          cumulativeFrequencyArray.push(currentFrequency)
+        }
+      })
+    }
+  }
+)};
+
+// day1part1()
+day1part2()
+
